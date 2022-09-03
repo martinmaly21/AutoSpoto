a5aa4a7c82f764c81f64008de8ed527c84873423
Martin Maly <martinmaly66@hotmail.com>
2022-09-03 17:28:11 -0400

    Initial Commit

---
+++ AutoSpoto/AutoSpotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoSpotoApp.swift
+//  AutoSpoto
+//
+//  Created by Martin Maly on 2022-09-03.
+//
+
+import SwiftUI
+
+@main
+struct AutoSpotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
