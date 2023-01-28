3075e79b23302617224c3bb22971b5adfdb9f7c2
Martin Maly <martinmaly66@hotmail.com>
2023-01-27 20:39:43 -0500

    Added custom fonts and colors

---
+++ AutoSpoto/Extensions/Color.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor.swift
+//  AutoSpoto
+//
+//  Created by Martin Maly on 2023-01-16.
+//
+
+import SwiftUI
+
+extension Color {
+    static let accentPink = Color("Accent Pink")
+    static let accentYellow = Color("Accent Yellow")
+    static let primaryBlue = Color("Primary Blue")
+    static let secondaryBlue = Color("Secondary Blue")
+    static let tertiaryBlue = Color("Tertiary Blue")
+}
---
+++ AutoSpoto/Extensions/Font.swift
@@ -0,0 +1,82 @@
+//
+//  Font.swift
+//  AutoSpoto
+//
+//  Created by Martin Maly on 2023-01-16.
+//
+
+import SwiftUI
+
+extension Font {
+    static func graphikBlack(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Black", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikBlackItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Black Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikBold(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Bold", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikBoldItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Bold Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikExtraLight(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Extra Light", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikExtraLightItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Extra Light Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikLight(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Light", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikLightItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Light Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikMedium(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Medium", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikMediumItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Medium Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikRegular(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Regular", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikRegularItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Regular Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikSemiBold(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Semi Bold", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikSemiBoldItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Semi Bold Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikSuper(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Super", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikSuperItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Super Italic", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikThin(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Thin", size: size, relativeTo: relativeTo)
+    }
+
+    static func graphikThinItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Graphik Thin Italic", size: size, relativeTo: relativeTo)
+    }
+}
