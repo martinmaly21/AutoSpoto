de38821918bcd7ec7e04287dbf0955c19d846c78
Martin Maly <martinmaly66@hotmail.com>
2023-01-30 19:31:59 -0500

    Continue building initial app structure; Disable resizing of window (at least during onboarding)

--- AutoSpoto/Extensions/Font.swift
+++ AutoSpoto/Extensions/Font.swift
@@ -8,75 +8,35 @@
 import SwiftUI
 
 extension Font {
-    static func graphikBlack(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Black", size: size, relativeTo: relativeTo)
+    static func josefinSansBold(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans Bold", size: size, relativeTo: relativeTo)
     }
 
-    static func graphikBlackItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Black Italic", size: size, relativeTo: relativeTo)
+    static func josefinSansBoldItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans Bold Italic", size: size, relativeTo: relativeTo)
     }
 
-    static func graphikBold(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Bold", size: size, relativeTo: relativeTo)
+    static func josefinSansLight(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans Light", size: size, relativeTo: relativeTo)
     }
 
-    static func graphikBoldItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Bold Italic", size: size, relativeTo: relativeTo)
+    static func josefinSansLightItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans Light Italic", size: size, relativeTo: relativeTo)
     }
 
-    static func graphikExtraLight(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Extra Light", size: size, relativeTo: relativeTo)
+    static func josefinSansRegular(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans Regular", size: size, relativeTo: relativeTo)
     }
 
-    static func graphikExtraLightItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Extra Light Italic", size: size, relativeTo: relativeTo)
+    static func josefinSansSemibold(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans SemiBold", size: size, relativeTo: relativeTo)
     }
 
-    static func graphikLight(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Light", size: size, relativeTo: relativeTo)
+    static func josefinSansThin(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans Thin", size: size, relativeTo: relativeTo)
     }
 
-    static func graphikLightItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Light Italic", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikMedium(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Medium", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikMediumItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Medium Italic", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikRegular(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Regular", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikRegularItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Regular Italic", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikSemiBold(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Semi Bold", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikSemiBoldItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Semi Bold Italic", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikSuper(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Super", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikSuperItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Super Italic", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikThin(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Thin", size: size, relativeTo: relativeTo)
-    }
-
-    static func graphikThinItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
-        return Font.custom("Graphik Thin Italic", size: size, relativeTo: relativeTo)
+    static func josefinSansThinItalic(_ size: CGFloat, relativeTo: TextStyle = .body) -> Font {
+        return Font.custom("Josefin Sans Thin Italic", size: size, relativeTo: relativeTo)
     }
 }
--- AutoSpoto/Supporting FIles/AutoSpotoConstants+Strings.swift
+++ AutoSpoto/Supporting FIles/AutoSpotoConstants+Strings.swift
@@ -20,10 +20,22 @@ extension AutoSpotoConstants.Strings {
         comment: "App name."
     )
 
+    static let AUTO_SPOTO_SPLASH_MOTTO = NSLocalizedString(
+        "AUTO_SPOTO_SPLASH_MOTTO",
+        value: "Never lose a reccomended song again.",
+        comment: "AutoSpoto motto."
+    )
+
     static let GET_STARTED = NSLocalizedString(
         "GET_STARTED",
         value: "Get started",
         comment: "Generic 'Get started' string."
+    )
+
+    static let MADE_WITH_LOVE = NSLocalizedString(
+        "MADE_WITH_LOVE",
+        value: "Made with ❤️ in 🇨🇦",
+        comment: "Text that's displayed at bottom of splash screen."
     )
 }
 
