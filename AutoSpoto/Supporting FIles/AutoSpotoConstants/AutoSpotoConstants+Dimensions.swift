f522fc37061cf836b75315a0efb8aa59eae1b8c1
Martin Maly <martinmaly66@hotmail.com>
2023-01-31 15:40:05 -0500

    Title animation kinda working

---
+++ AutoSpoto/Supporting FIles/AutoSpotoConstants/AutoSpotoConstants+Dimensions.swift
@@ -0,0 +1,19 @@
+//
+//  AutoSpotoConstants+Dimensions.swift
+//  AutoSpoto
+//
+//  Created by Martin Maly on 2023-01-31.
+//
+
+import Foundation
+
+extension AutoSpotoConstants {
+    struct Dimensions {
+        private init() {}
+    }
+}
+
+extension AutoSpotoConstants.Dimensions {
+    static let onboardingWindowWidth: CGFloat = 1000
+    static let onboardingWindowHeight: CGFloat = 600
+}
