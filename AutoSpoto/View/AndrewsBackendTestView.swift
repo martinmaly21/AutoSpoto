31b61913393d4d9984dbd8a6c461e11693b02b1b
Andrew <acarava3@uwo.ca>
2023-02-03 16:54:54 -0500

    Added Swift interface for uploading songs to a playlist in spotify spotify

--- AutoSpoto/View/AndrewsBackendTestView.swift
+++ AutoSpoto/View/AndrewsBackendTestView.swift
@@ -13,14 +13,14 @@ struct AndrewsBackendTestView: View {
     var body: some View {
         VStack{
             Button(action:{
-                SwiftPythonInterface()
+                AddSongsToPlaylist()
                 showResult.toggle()
             },
                    label:{
                 Text("My Button")
             })
             if showResult{
-                Text(String("\(SwiftPythonInterface())"))
+                Text(String("\(AddSongsToPlaylist())"))
             }
         }
     }
