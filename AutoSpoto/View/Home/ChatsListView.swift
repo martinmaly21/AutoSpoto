66557ef223ca5593c4d5bfb280ab9c6ad7bb46da
Martin Maly <martinmaly66@hotmail.com>
2023-02-11 17:01:56 -0500

    Update UI of ChatsListView.swift

--- AutoSpoto/View/Home/ChatsListView.swift
+++ AutoSpoto/View/Home/ChatsListView.swift
@@ -12,15 +12,36 @@ struct ChatsListView: View {
     @Binding var selectedChat: Chat?
 
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(chats, id: \.self) { chat in
-                    ChatRow(chat: chat, isSelected: chat == selectedChat)
-                        .onTapGesture {
-                            selectedChat = chat
-                        }
+        let topInset: CGFloat = 45
+
+        ZStack(alignment: .top) {
+            ScrollView {
+                Spacer()
+                    .frame(height: topInset)
+                VStack {
+                    ForEach(chats, id: \.self) { chat in
+                        ChatRow(chat: chat, isSelected: chat == selectedChat)
+                            .onTapGesture {
+                                selectedChat = chat
+                            }
+                    }
                 }
             }
+            .introspectScrollView { scrollView in
+                scrollView.scrollerInsets = NSEdgeInsets(top: topInset, left: 0, bottom: 0, right: 0)
+            }
+
+            HStack {
+                Text(AutoSpotoConstants.Strings.AUTO_SPOTO_APP_NAME)
+                    .font(.josefinSansBold(24))
+                    .foregroundColor(.white)
+                    .padding(.leading, 18)
+                    .padding(.top, 6)
+                    .padding(.bottom, 10)
+
+                Spacer()
+            }
+            .background(.ultraThinMaterial)
         }
     }
 }
