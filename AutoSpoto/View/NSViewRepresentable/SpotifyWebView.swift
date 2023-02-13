ab474d61b3ae8fde197fe4b96c35ae0bca0917d8
Martin Maly <martinmaly66@hotmail.com>
2023-02-12 20:05:17 -0500

    Spotify login appears to be working

--- AutoSpoto/View/NSViewRepresentable/SpotifyWebView.swift
+++ AutoSpoto/View/NSViewRepresentable/SpotifyWebView.swift
@@ -39,18 +39,78 @@ struct SpotifyWebView: NSViewRepresentable {
             decisionHandler(.allow)
         }
         
-        func requestForCallbackURL(request: URLRequest) {
+        private func requestForCallbackURL(request: URLRequest) {
             guard let requestURLString = request.url?.absoluteString else {
                 return
             }
 
-
             if requestURLString.hasPrefix(redirectURI) {
-                let code = requestURLString.getQueryStringParameter(param: "code")
+                guard let code = requestURLString.getQueryStringParameter(param: "code") else {
+                    fatalError("Could not get code from requestURLString")
+                }
 
-                //Do next shit
-                print("Test: \(code)")
+                sendPostRequest(code: code) { (data, response, error) in
+                    if let error = error {
+                        //TODO: handle error better?
+                        print("Error: \(error)")
+                        return
+                    }
+
+                    guard let data = data, let response = response as? HTTPURLResponse else {
+                        //TODO: handle error better?
+                        print("Error: No data or response")
+                        return
+                    }
+
+                    if response.statusCode != 200 {
+                        //TODO: handle error better?
+                        print("Error: Response code is not 200")
+                        return
+                    }
+
+                    do {
+                        let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+
+                        if let JSONString = String(data: data, encoding: String.Encoding.utf8) {
+                            self.parent.spotifyAccessToken = JSONString
+                        }
+                    } catch {
+                        //TODO: handle error better?
+                        print("Error: Unable to parse JSON")
+                    }
+                }
             }
+        }
+
+        private func sendPostRequest(code: String, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+            let authOptions: [String: Any] = [
+                "form": [
+                    "grant_type": "authorization_code",
+                    "redirect_uri": redirectURI,
+                    "code": code,
+                ],
+                "headers": [
+                    "Authorization": "Basic "
+                    + (Data("\(clientID):\(clientSecret)".utf8).base64EncodedString())
+                ],
+                "json": true,
+            ]
+
+            var request = URLRequest(url: AutoSpotoConstants.URL.endpoint)
+            request.httpMethod = "POST"
+
+            if let headers = authOptions["headers"] as? [String: String] {
+                for (headerField, headerValue) in headers {
+                    request.setValue(headerValue, forHTTPHeaderField: headerField)
+                }
+            }
+
+            if let form = authOptions["form"] as? [String: String] {
+                let formData = form.map { "\($0)=\($1)" }.joined(separator: "&").data(using: .utf8)
+                request.httpBody = formData
+            }
+
+            URLSession.shared.dataTask(with: request, completionHandler: completion).resume()
         }
     }
 
