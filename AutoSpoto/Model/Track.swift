2a80d521272a75ae1453e2287b09aa824aa4b336
Martin Maly <martinmaly66@hotmail.com>
2023-03-15 16:57:18 -0400

    Remove OpenGraph package; Remove ‘fetchTrackMetadata’ in Track.swift

--- AutoSpoto/Model/Track.swift
+++ AutoSpoto/Model/Track.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import OpenGraph
 
 class Track: Hashable {
     var url: URL
@@ -25,8 +24,6 @@ class Track: Hashable {
     var title: String?
     var artist: String?
 
-    private var currentTask: URLSessionDataTask?
-
     init?(trackCodable: TrackCodable) {
         guard let url = URL(string: trackCodable.decoded_blob) else {
             assertionFailure("Could not create URL")
@@ -34,61 +31,6 @@ class Track: Hashable {
         }
         self.url = url
         self.timeStamp = trackCodable.date_utc
-    }
-
-    public func fetchTrackMetadata(
-        completion: @escaping (Track) -> Void
-    ) {
-        guard !hasFetchedMetadata && !isFetchingMetadata else { return }
-
-        isFetchingMetadata = true
-        hasFetchedMetadata = true
-
-        let task = OpenGraph.fetch(
-            url: url,
-            completion: { result in
-                self.currentTask = nil
-
-                switch result {
-                case .failure(let error):
-
-                    if let error = error as? OpenGraphResponseError,
-                       case .unexpectedStatusCode(404) = error {
-                        //404 error (invalid url)
-                        self.errorFetchingMetadata = .error404
-                        self.hasFetchedMetadata = true
-                    } else {
-                        //otherwise, fetch was cancelled or perhaps too many network requests. Either way, we can
-                        //try to fetch again next time the cell appears
-                        self.hasFetchedMetadata = false
-                    }
-
-                    self.isFetchingMetadata = false
-
-                    completion(self)
-                case .success(let og):
-                    if let imageURLString = og[.image] {
-                        self.imageURL = URL(string: imageURLString)
-                    }
-
-                    self.title = og[.title]
-                    self.artist = og[.description]
-                    self.isFetchingMetadata = false
-
-                    completion(self)
-                }
-            }
-        )
-
-        if let currentTask = currentTask {
-            currentTask.cancel()
-        }
-
-        self.currentTask = task
-    }
-
-    public func cancelTaskIfNeeded() {
-        currentTask?.cancel()
     }
 
     func hash(into hasher: inout Hasher) {
