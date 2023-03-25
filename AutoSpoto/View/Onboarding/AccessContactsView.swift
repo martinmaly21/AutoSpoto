670108720578c0b51833c131cd8b7bb094ef4922
Andrew <acarava3@uwo.ca>
2023-03-25 12:39:56 -0400

    added contacts view in onboarding

---
+++ AutoSpoto/View/Onboarding/AccessContactsView.swift
@@ -0,0 +1,20 @@
+//
+//  AccessContactsView.swift
+//  AutoSpoto
+//
+//  Created by Andrew Caravaggio on 2023-03-25.
+//
+
+import SwiftUI
+
+struct AccessContactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccessContactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccessContactsView()
+    }
+}
