fc3f69de21863b7d44f27860eec22bc538e00815
Martin Maly <martinmaly66@hotmail.com>
2023-06-01 17:51:30 +0900

    Updated Chat.swift and IndividualChatCodable.swift to decode new JSON; Call new methods from HomeViewModel.swift

--- AutoSpoto/Model/Codable/IndividualChatCodable.swift
+++ AutoSpoto/Model/Codable/IndividualChatCodable.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct IndividualChatCodable: Codable {
-    let Image: String?
-    let Phone_Number: String
-    let chat_ids: [Int]
-    let First_Name: String?
-    let Last_Name: String?
-    let playlist_id: String?
+    let imageBlob: String?
+    let contactInfo: String
+    let chatIDs: [Int]
+    let firstName: String?
+    let lastName: String?
+    let spotifyPlaylistID: String?
 }
     
