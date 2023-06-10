31be73545937e6d924d74306f728863c848ee594
Martin Maly <martinmaly66@hotmail.com>
2023-06-10 21:14:38 +0900

    Update UI of button in ChatView if playlist exists

--- AutoSpoto/View/Helper Views/ModifyPlaylistButton.swift
+++ AutoSpoto/View/Helper Views/ModifyPlaylistButton.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ModifyPlaylistButton: View {
+    let playlistExists: Bool
     let action: () -> Void
 
     var body: some View {
@@ -15,11 +16,11 @@ struct ModifyPlaylistButton: View {
             action: action,
             label: {
                 HStack(spacing: 12) {
-                    Image(systemName: "plus.circle")
+                    Image(systemName: playlistExists ? "pencil.circle" : "plus.circle")
                         .resizable()
                         .frame(width: 22, height: 22)
 
-                    Text(AutoSpotoConstants.Strings.CREATE_PLAYLIST)
+                    Text(playlistExists ? AutoSpotoConstants.Strings.MODIFY_PLAYLIST : AutoSpotoConstants.Strings.CREATE_PLAYLIST)
                         .font(.josefinSansSemibold(18))
                 }
                 .frame(height: 50)
@@ -27,7 +28,7 @@ struct ModifyPlaylistButton: View {
                 .background(Color.primaryBlue)
                 .foregroundColor(Color.textPrimaryWhite)
                 .clipShape(Capsule())
-                .shadow(color: .black.opacity(0.5), radius: 6, x: 4, y: 4)
+                .shadow(color: .black.opacity(0.2), radius: 6, x: 4, y: 4)
             }
         )
         .buttonStyle(.plain)
