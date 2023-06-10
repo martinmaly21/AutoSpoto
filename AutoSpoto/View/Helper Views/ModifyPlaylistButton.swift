d2d7cc37358dfcc78074f06cd4ef42effa84823b
Martin Maly <martinmaly66@hotmail.com>
2023-06-10 23:58:25 +0900

    Updated UX for when you create a playlist

--- AutoSpoto/View/Helper Views/ModifyPlaylistButton.swift
+++ AutoSpoto/View/Helper Views/ModifyPlaylistButton.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ModifyPlaylistButton: View {
     let spotifyPlaylist: SpotifyPlaylist?
@@ -22,17 +23,32 @@ struct ModifyPlaylistButton: View {
                         .shadow(color: .black.opacity(0.2), radius: 6, x: 4, y: -3)
                     
                     HStack(spacing: 12) {
-                        Image("spotify-logo")
-                            .resizable()
-                            .renderingMode(.template)
-                            .foregroundColor(.textPrimaryWhite)
-                            .frame(width: 30, height: 30)
+                        if let imageURL = spotifyPlaylist?.imageURL {
+                            KFImage(imageURL)
+                                .placeholder {
+                                    Color.gray
+                                        .frame(width: 60, height: 40)
+                                        .cornerRadius(8)
+                                }
+                                .cacheOriginalImage(true)
+                                .fade(duration: 0.25)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 60, height: 40)
+                                .cornerRadius(8)
+                                .aspectRatio(contentMode: .fill)
+                        }
                         
-                        Text(spotifyPlaylist?.id ?? AutoSpotoConstants.Strings.MODIFY_PLAYLIST)
-                            .font(.josefinSansSemibold(18))
+                        if let name = spotifyPlaylist?.name {
+                            Text(String.localizedStringWithFormat(AutoSpotoConstants.Strings.MODIFY_PLAYLIST_WITH_NAME, name))
+                                .font(.josefinSansSemibold(18))
+                        } else {
+                            Text(AutoSpotoConstants.Strings.MODIFY_PLAYLIST)
+                                .font(.josefinSansSemibold(18))
+                        }
                     }
                     .padding(.horizontal, 30)
-                    
+                    .redacted(reason: spotifyPlaylist == nil ? .placeholder : [])
                     .foregroundColor(Color.textPrimaryWhite)
                 }
                 .frame(height: height)
