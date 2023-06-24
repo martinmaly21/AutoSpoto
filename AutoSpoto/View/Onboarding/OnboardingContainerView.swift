92bc8085b4f57c8b83db1cf5f0589d5a026d7997
Martin Maly <martinmaly66@hotmail.com>
2023-06-24 12:00:39 +0900

    Fixed bug where permissions success label would be shown prematurely

--- AutoSpoto/View/Onboarding/OnboardingContainerView.swift
+++ AutoSpoto/View/Onboarding/OnboardingContainerView.swift
@@ -89,7 +89,7 @@ struct OnboardingContainerView: View {
             case .permissionsRequestView:
                 PermissionsRequestView(
                     userAuthorizedSpotify: $userAuthorizedSpotify,
-                    userAuthorizedDiskAcess: $userAuthorizedDiskAccess
+                    userAuthorizedDiskAccess: $userAuthorizedDiskAccess
                 )
             }
             
