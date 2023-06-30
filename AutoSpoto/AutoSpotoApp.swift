58399cfb5272d1b7a3f854db2e039c697ba94afe
Martin Maly <martinmaly66@hotmail.com>
2023-06-30 18:30:12 +0900

    Added template code from ‘https://sparkle-project.org/documentation/programmatic-setup/‘

--- AutoSpoto/AutoSpotoApp.swift
+++ AutoSpoto/AutoSpotoApp.swift
@@ -6,13 +6,25 @@
 //
 
 import SwiftUI
+import Sparkle
 
 @main
 struct AutoSpotoApp: App {
+    private let updaterController: SPUStandardUpdaterController
+    
+    init() {
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    }
+    
     var body: some Scene {
         WindowGroup {
             AutoSpotoContainerView()
                 .preferredColorScheme(.dark)
+        }
+        .commands {
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: updaterController.updater)
+            }
         }
         //disable resizing of the window
         .windowResizability(.contentSize)
